b2ce1f2ceb4a363f407c2394affdaaba747eeb28
Edwin Divine <omegbuedwin@gmail.com>
2023-05-13 11:30:49 +0100

    major correction

--- index.js
+++ index.js
@@ -12,16 +12,31 @@ function mode() {
 mode()
 let countries = document.getElementById('countries')
 console.time('one')
- file.forEach(e => {
-    countries.innerHTML += 
-        `<div class="country">
-            <img class="c-flag" src="${e.flag}" alt="">
-            <div class="desc">
-                <h2 class="c-name">${e.name}</h2>
-                <p class="c-population"><span class="bold">Population:</span> ${e.population}</p>
-                <p class="c-region"><span \----\---class="bold">Region:</span>${e.region}</p>
-                <p class="c-capital"><span class="bold">Capital:</span>${e.capital}</p>
-            </div>
-        </div>`
-});
+let countriesString = ""
+for (let i = 0; i < file.length; i++) {
+    countriesString +=
+            `<div class="country">
+                <img class="c-flag" src="${file[i].flag}" alt="">
+                <div class="desc">
+                    <h2 class="c-name">${file[i].name}</h2>
+                    <p class="c-population"><span class="bold">Population:</span> ${file[i].population}</p>
+                    <p class="c-region"><span class="bold">Region:</span>${file[i].region}</p>
+                    <p class="c-capital"><span class="bold">Capital:</span>${file[i].capital}</p>
+                </div>
+            </div>`
+}
+countries.innerHTML = countriesString
+//     countries.innerHTML += 
+//         `<div class="country">
+//             <img class="c-flag" src="${e.flag}" alt="">
+//             <div class="desc">
+//                 <h2 class="c-name">${e.name}</h2>
+//                 <p class="c-population"><span class="bold">Population:</span> ${e.population}</p>
+//                 <p class="c-region"><span class="bold">Region:</span>${e.region}</p>
+//                 <p class="c-capital"><span class="bold">Capital:</span>${e.capital}</p>
+//             </div>
+//         </div>`
+// });
 console.timeEnd('one')
+
+
