9e779c28d93af31df7f0a50c50029d040b8cfd37
Edwin Divine <omegbuedwin@gmail.com>
2023-05-13 10:33:13 +0100

    minor correction

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ function mode() {
 
 }
 mode()
+let countries = document.getElementById('countries')
 console.time('one')
  file.forEach(e => {
-    let countries = document.getElementById('countries')
     countries.innerHTML += 
         `<div class="country">
             <img class="c-flag" src="${e.flag}" alt="">
             <div class="desc">
                 <h2 class="c-name">${e.name}</h2>
                 <p class="c-population"><span class="bold">Population:</span> ${e.population}</p>
-                <p class="c-region"><span class="bold">Region:</span>${e.region}</p>
+                <p class="c-region"><span \----\---class="bold">Region:</span>${e.region}</p>
                 <p class="c-capital"><span class="bold">Capital:</span>${e.capital}</p>
             </div>
         </div>`
