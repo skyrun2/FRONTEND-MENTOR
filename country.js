20c620ba1e73e4ef2028b2d91e580a10c254ac2e
Edwin Divine <omegbuedwin@gmail.com>
2023-08-06 11:51:51 +0100

    country

---
+++ country.js
@@ -0,0 +1,65 @@
+import file from "./rest-countries-api-with-color-theme-switcher-master/data.json" assert {type: "json"}
+function mode() {
+    let mode = document.getElementById('mode')
+    let  body = document.getElementById('body')
+    mode.addEventListener('click' ,function(e){
+        body.classList.toggle('dm')
+    })
+
+}
+mode()
+console.log(file)
+
+function getQeuryParam(str) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(str);
+}
+
+const seachQuery =  getQeuryParam('country')
+console.log(seachQuery)
+let countryInfo = document.getElementById('country-info')
+let countryobj = {}
+
+for (const country of file) {
+    if (country.name == seachQuery) {
+        countryobj = country
+    }
+}
+console.log(countryobj)
+document.title = countryobj.name
+ function placement(obj) {
+    let countryString =''
+    console.log(obj.name);
+    countryString = 
+    `<div class="left">
+    <div class="prev  "><a href="index.html" class="back-link  txt">Back</a></div>
+    <div class="img-ctrl"><img src="${obj.flag}" class="country-flag" alt=""></div>
+</div>
+<div class="right">
+    <h2 class="country-name txt">${obj.name}</h2>
+    <div class="desc">
+        <p class="nat-name   txt">Native Name: <span id="nn">${obj.nativeName}</span></p>
+        <p class="popl   txt">Population: <span id="pp">${obj.population}</span></p>
+        <p class="region   txt">Region: <span ids="rg">${obj.region}</span></p>
+        <p class="sub-reg   txt">Sub-Region: <span id="sb-rg">${obj.subregion}</span></p>
+        <p class="capital   txt">Capital: <span id="cap">${obj.capital}</span></p>
+        <p class="top-lv-dom   txt">Top-Level-Domain: <span id="tp-lv-dom">${obj.topLevelDomain[0]}</span></p>
+        <p class="cur   txt">Currencies: <span id="cur">${obj.currencies[0].name}</span></p>
+        <p class="lang   txt">Languages <span id="lang">${obj.languages[0].name}</span></p>
+    </div>
+    <div class="border">
+    <p class="bord-count txt  ">Border Countries</p>
+    <div class="borded " id="borded">
+    </div>
+    </div>
+    </div>`
+    countryInfo.innerHTML = countryString
+    let borders = document.getElementById('borded')
+    let borderList = ''
+    console.log(obj.borders);
+    borderList = obj.borders.map((e,i,arr) =>{`<div class="count txt">${arr[i]}</div>`})
+    console.log(borderList);
+    borders.innerHTML = borderList
+ }
+ placement(countryobj)
+ 
--- index.js
+++ index.js
@@ -1,29 +1,16 @@
  import file from "./rest-countries-api-with-color-theme-switcher-master/data.json" assert {type: "json"}
-console.log(file);
 function mode() {
     let mode = document.getElementById('mode')
     let  body = document.getElementById('body')
     mode.addEventListener('click' ,function(e){
         body.classList.toggle('dm')
-        console.log(body.classList);
     })
 
 }
+console.log(file);
 mode()
 let filter = document.getElementById('filter')
 let continents = document.getElementById('continents')
-// function dropDown(){
-//     if (continents.style.display = `none`) {
-//         filter.addEventListener('mouseover', ()=>{
-//             continents.style.display = `flex`
-//             console.log('1');})
-//     }
-//     if (continents.style.display = `flex`) {
-//         filter.addEventListener('click', ()=>{continents.style.display = `none`
-//         console.log('2')})
-//     }
-// }
-// dropDown()
 let countries = document.getElementById('countries')
 let asia = document.getElementById('asia')
 let africa = document.getElementById('africa')
@@ -41,8 +28,11 @@ let allString = file
  let polasString = file.filter(item=> item.region == 'Polar')
  let europeString = file.filter(item=> item.region == 'Europe')
  let oceaniaString = file.filter(item=> item.region == 'Oceania')
-
- console.log(allString, asiaString, africaString, americasString, polasString,europeString, oceaniaString)
+ let searchInput = ''
+ let searchIcon = document.getElementById('search-icon')
+ let searchBar = document.getElementById('search-bar')
+ 
+ 
 function placement(arr) {
     let countriesString = ""
     for (let i = 0; i <arr.length; i++) {
@@ -54,8 +44,10 @@ function placement(arr) {
                         <p class="c-population"><span class="bold">Population:</span> ${arr[i].population}</p>
                         <p class="c-region"><span class="bold">Region:</span>${arr[i].region}</p>
                         <p class="c-capital"><span class="bold">Capital:</span>${arr[i].capital}</p>
-                    </div>
+                        </div>
+                        <a href="country.html?country=${arr[i].name}" class="q-p" id="q-p" ></a>
                 </div>`
+                console.log(arr[i].name)
     
     }
     countries.innerHTML = countriesString
@@ -67,10 +59,30 @@ console.log(placement(file));
  all.addEventListener('click',()=>{placement(allString)})
  asia.addEventListener('click',(()=>{placement(asiaString)}))
  africa.addEventListener('click',(()=>{placement(africaString)}))
- europe.addEventListener('click',(()=>{placement(europe)}))
+ europe.addEventListener('click',(()=>{placement(europeString)}))
  ociena.addEventListener('click',(()=>{placement(oceaniaString)}))
  polar.addEventListener('click',(()=>{placement(polasString)}))
+ americas.addEventListener('click',(()=>{placement(americasString)}))
  
+ searchBar.addEventListener('input',(()=>{
+    searchBar.value = searchBar.value.replace(/[^a-zA-Z \-]/g, '')
+    searchInput = searchBar.Value
+}))
 
+searchIcon.addEventListener('click',(()=>{
+    console.log(searchInput)
+    console.log(searchBar.value);
+    let search = file.filter(item =>item.name.toLowerCase().includes(searchBar.value.toLowerCase()))
+    console.log(search);
+    placement(search)
+}))
 
- 
+searchBar.addEventListener('focus',(()=>{
+    document.body.onkeydown = function(e) {
+        if (e.key == "Enter" || e.code == "Enter" || e.keyCode == 13){
+            let search = file.filter(item =>item.name.toLowerCase().includes(searchBar.value.toLowerCase()))
+            console.log(search);
+            placement(search)
+        }
+    }
+}))
